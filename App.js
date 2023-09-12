54d4e5c8924661d11bd423db6921412731b883c8
Pravin <pravinmishrag3@gmail.com>
2023-09-12 23:07:55 +0530

    episode three Laying the foundation

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-// Write simple program using JavaScript
+// React.createElement => ReactElement(object) => HTML Elememnt (render)
 
-// const heading = document.createElement("h1");
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namste JavaScript"
+// );
+// console.log(heading);
 
-// heading.innerHTML = "I am Hading from JavaScript";
+// JSX (transpiled before it reachses to the JS)=> parcel=>Babel 
+// JSX => bable transile it to React.createElement=>ReactElement(object)=>HTML Element (render)
 
-// const root = document.getElementById("root");
+// React Element-
+const Title =()=> <h1 id="heading">I am a JSX</h1>;
+// It is end of the day JavaScript object
 
-// root.appendChild(heading)
 
 
-// Write a simple program using React CDN 
 
-const heading1 = React.createElement("div",{id:"parent"},[
-    React.createElement("h1",{},"I am a good engineer"),
-    React.createElement("h2",{},"I am h2 Tag")
-])
-    const root1 = ReactDOM.createRoot(document.getElementById("root"));
-    root1.render(heading1)
+// React Component
+// 1) Class based component
+// 2) Functional based component
+
+// React Functioanl component
+
+// First way
+
+// const fn = ()=>{
+//   return true
+// }
+
+// Second way
+
+// const fn = ()=>true
+
+// bothe are the same code
+
+const FunctionalComponent = ()=>{
+ return(
+  <div>
+  {/* Component comsition */}
+  <Title/>
+  <h1>Do not panic to see such syntax my dear friesn</h1>
+  <h2>We can write multiple things </h2>
+  <h3>Write nested element without return keyword</h3>
+</div>
+  )
+}
+
+// const FunctionalComponent = ()=>(<div>
+//   <h1>Do not panic to see such syntax my dear friesn</h1>
+//   <h2>We can write multiple things </h2>
+//   <h3>Write nested element without return keyword</h3>
+// </div>)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponent/>);
